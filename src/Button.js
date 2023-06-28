3addf5f2bb5ef453990b4f2c0096cecd1e42159b
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2023-06-28 21:10:50 +0200

    refactor: Add comments to button.js

--- src/Button.js
+++ src/Button.js
@@ -1,4 +1,14 @@
+/**
+ * Button component
+ *
+ * @param {Object} props Button props
+ * @param {Function} props.onClick Callback fired when button is clicked
+ * @param {String|Element} props.children Child element(s) to render inside button
+ *
+ * @returns {Element} Button element
+ */
 export function Button({ children, onClick }) {
+  /** @returns Button element */
   return (
     <button className="button" onClick={onClick}>
       {children}
