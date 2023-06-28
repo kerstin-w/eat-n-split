f762e96e05df4cee9cb4af51149dcaadf08f3c28
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2023-06-28 21:13:32 +0200

    refactor: Add comments to FormAddfriends.js

--- src/FormAddfriend.js
+++ src/FormAddfriend.js
@@ -1,15 +1,27 @@
 import { useState } from "react";
 import { Button } from "./Button";
 
+/**
+ * Form for adding a new friend
+ *
+ * @param {Object} props Component props
+ * @param {Function} props.onAddFriend Called when form is submitted
+ */
 export function FormAddfriend({ onAddFriend }) {
+  /** State for form input values */
   const [name, setName] = useState("");
   const [image, setImage] = useState("https://i.pravatar.cc/48");
 
+  /**
+   * Handle form submission
+   * @param {Event} e Form submit event
+   */
   function handleSubmit(e) {
     e.preventDefault();
 
     if (!name || !image) return;
 
+    // Generate ID and create new friend object
     const id = crypto.randomUUID();
     const newFriend = {
       id,
@@ -18,8 +30,10 @@ export function FormAddfriend({ onAddFriend }) {
       balance: 0,
     };
 
+    // Call callback to add new friend
     onAddFriend(newFriend);
 
+    // Reset form state  to initial values
     setName("");
     setImage("https://i.pravatar.cc/48");
   }
