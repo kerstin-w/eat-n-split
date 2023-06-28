d09f17273ae59d85417d200c3aa4ab9155cd4976
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2023-06-28 21:20:05 +0200

    refactor: Add comments to FriendsList.js

--- src/FriendsList.js
+++ src/FriendsList.js
@@ -1,6 +1,20 @@
 import { Friend } from "./Friend";
 
+/**
+ * Component to display a list of friends
+ *
+ * @param {Object} props Component props
+ * @param {Array} props.friends Array of friend objects
+ * @param {Function} props.onSelection Called when a friend is selected
+ * @param {Object} props.selectedFriend Currently selected friend
+ *
+ * @returns {Element} UL element containing Friend components
+ */
 export function FriendsList({ friends, onSelection, selectedFriend }) {
+  /**
+   * Render Friend component for each friend
+   * Pass relevant props to Friend component
+   */
   return (
     <ul>
       {friends.map((friend) => (
